25240b6ea9484b62955c81219b679dd040e12d5b
Mark Connell <mark@markconnell.co.uk>
2010-06-21 13:15:21 +0100

    Define an accessible instance method current subdomain for the controller.

--- lib/multi_tenant_controller.rb
+++ lib/multi_tenant_controller.rb
@@ -2,6 +2,7 @@ module MultiTenantController
   def subdomain_by_multi_tenant_model(tenant = :account)
     self.class_eval do
       cattr_accessor :tenant, :tenant_class
+      attr_accessor :current_subdomain
     end
 
     self.tenant_class = tenant.to_s.capitalize.constantize
@@ -10,7 +11,7 @@ module MultiTenantController
       before_filter :find_account_from_subdomain
       def session
         if tenant_class.current.present?
-          request.session[@current_subdomain] ||= {}
+          request.session[current_subdomain] ||= {}
         else
           request.session
         end
@@ -18,7 +19,7 @@ module MultiTenantController
 
       def session=(*args)
         if tenant_class.present?
-          request.session[@current_subdomain] = args
+          request.session[current_subdomain] = args
         else
           request.session = args
         end
@@ -26,8 +27,8 @@ module MultiTenantController
 
       private
       def find_account_from_subdomain
-        @current_subdomain = request.host.split(/\./).first
-        tenant_class.current = tenant_class.find_by_subdomain!(@current_subdomain)
+        self.current_subdomain = request.host.split(/\./).first
+        tenant_class.current = tenant_class.find_by_subdomain!(current_subdomain)
       end
     end
   end
