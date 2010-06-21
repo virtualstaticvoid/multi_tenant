fca68a31b2776a3a7d0374c7469522856dcce4e4
Mark Connell <mark@markconnell.co.uk>
2010-06-21 21:53:14 +0100

    drop multi sessions for the moment, issue with csrf token.

--- lib/multi_tenant_controller.rb
+++ lib/multi_tenant_controller.rb
@@ -9,21 +9,6 @@ module MultiTenantController
 
     self.class_eval do
       before_filter :find_account_from_subdomain
-      def session
-        if tenant_class.current.present?
-          request.session[current_subdomain] ||= {}
-        else
-          request.session
-        end
-      end
-
-      def session=(*args)
-        if tenant_class.present?
-          request.session[current_subdomain] = args
-        else
-          request.session = args
-        end
-      end
 
       private
       def find_account_from_subdomain
