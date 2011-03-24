857fc80c314b24b4f6b55143894f04dce6d25179
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-03-24 16:36:17 +0200

    Refactored controller support, added some comments

--- lib/multi_tenant_controller.rb
+++ lib/multi_tenant_controller.rb
@@ -1,20 +1,32 @@
 module MultiTenantController
-  def subdomain_by_multi_tenant_model(tenant = :account)
+  
+  # adds multi-tenant support to respective controller
+  def multi_tenant_model(tenant = :account)
     self.class_eval do
       cattr_accessor :tenant, :tenant_class
-      attr_accessor :current_subdomain
     end
 
     self.tenant_class = tenant.to_s.capitalize.constantize
+  end
 
+  # adds multi-tenant support to respective controller based on the current subdomain
+  def subdomain_by_multi_tenant_model(tenant = :account)
+    multi_tenant_model(tenant)
     self.class_eval do
+      attr_accessor :current_subdomain
       before_filter :find_account_from_subdomain
 
       private
       def find_account_from_subdomain
+
+        # TODO: ensure "subdomain" isn't www or the actual domain
+        # consider using request.subdomain (rails 3 feature)        
+
         self.current_subdomain = request.host.split(/\./).first
         tenant_class.current = tenant_class.find_by_subdomain!(current_subdomain)
       end
     end
   end
+
 end
+
--- lib/multi_tenant_model.rb
+++ lib/multi_tenant_model.rb
@@ -1,11 +1,15 @@
 module MultiTenantModel
+
+  # adds "current" to the model which represents the tenant
   def multi_tenant_model
     self.class_eval do
       cattr_accessor :current
     end
   end
 
+  # adds multi-tenant support to respective model
   def belongs_to_tenant(tenant = :account, options = {})
+    
     self.class_eval do
       cattr_accessor :tenant, :tenant_class
     end
@@ -14,6 +18,7 @@ module MultiTenantModel
     self.tenant_class ||= tenant.to_s.capitalize.constantize
 
     self.class_eval do
+    
       belongs_to tenant, options
 
       def self.unscoped
@@ -32,6 +37,9 @@ module MultiTenantModel
         end
         super(attributes)
       end
+
     end
+
   end
 end
+
